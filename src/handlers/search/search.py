c8d6b915077e1ab8f91f6a747d40a6cf99ed3245
al <efimov_as@tochka.com>
2025-08-25 00:20:38 +0500

    search services

--- src/handlers/search/search.py
+++ src/handlers/search/search.py
@@ -7,16 +7,21 @@ from aiogram_dialog.widgets.input import TextInput, ManagedTextInput, MessageInp
 from aiogram_dialog.widgets.markup.reply_keyboard import ReplyKeyboardFactory
 
 from src.database.requests_db import ReqData
+from src.handlers.menu.start.start_state import StartDialog
 from src.handlers.search.search_state import SearchSpecialistDialog
 
 async def getter_categories(dialog_manager: DialogManager, **kwargs):
     req = ReqData()
-    res = await req.get_categories()
-    print('res', res)
+    res = await req.get_categories(is_new=False)
     return {'categories': res}
 
-async def select_category(callback: CallbackQuery, button: Button, dialog_manager: DialogManager, item_id: str):
-    print(item_id)
+async def select_category(callback: CallbackQuery, button: Button, dialog_manager: DialogManager, item_id: int):
+    dialog_manager.dialog_data['category'] = item_id
+    await dialog_manager.switch_to(SearchSpecialistDialog.service)
+
+async def back_to_start(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
+    await dialog_manager.done()
+    await dialog_manager.start(StartDialog.start, mode=StartMode.RESET_STACK)
 
 window_categories = Window(
     Format("Выберите категорию"),
@@ -30,13 +35,78 @@ window_categories = Window(
         ),
         width=2
     ),
+    Button(Const("Назад"), id="back_to_start", on_click=back_to_start),
     state=SearchSpecialistDialog.category,
     getter=getter_categories
 
 )
 
+async def getter_services(dialog_manager: DialogManager, **kwargs):
+    category_id = dialog_manager.dialog_data.get('category')
+
+    req = ReqData()
+    res = await req.get_services_by_category(category_id=int(category_id), is_new=False)
+
+    return {'services': res}
+
+async def select_service(callback: CallbackQuery, button: Button, dialog_manager: DialogManager, item_id: int):
+    dialog_manager.dialog_data['service'] = item_id
+    await dialog_manager.switch_to(SearchSpecialistDialog.specialists)
+
+
+window_services = Window(
+    Format("Выберите услугу"),
+    Group(
+        Select(
+            Format('{item.name}'),
+            id='service',
+            item_id_getter=lambda x: x.id,
+            items='services',
+            on_click=select_service
+        ),
+        width=2
+    ),
+    Back(Const("Назад"), id="back_to_category_search"),
+    state=SearchSpecialistDialog.service,
+    getter=getter_services
+)
+
+
+async def getter_specialists(dialog_manager: DialogManager, **kwargs):
+    service_id = dialog_manager.dialog_data.get('service')
+
+    req = ReqData()
+    res = await req.get_specialists_by_service(service_id=int(service_id))
+    print(res)
+    return {'specialists': res}
+
+async def select_specialist(callback: CallbackQuery, button: Button, dialog_manager: DialogManager, item_id: int):
+    print(item_id)
+
+window_specialists = Window(
+    Format("Выберите специалиста"),
+    Group(
+        Select(
+            Format('{item.name}'),
+            id='specialist',
+            item_id_getter=lambda x: x.id,
+            items='specialists',
+            on_click=select_specialist
+        ),
+        width=2
+    ),
+    Back(Const("Назад"), id="back_to_service_search"),
+    state=SearchSpecialistDialog.specialists,
+    getter=getter_specialists
+)
+
+
+
+
 dialog_search = Dialog(
-    window_categories
+    window_categories,
+    window_services,
+window_specialists
 )
 
 
